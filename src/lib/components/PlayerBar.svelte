f7b6a0f307d761181647e992e6b2068dc7dd1861
hammsvietro <hammsvietro@gmail.com>
2024-04-09 21:38:53 -0300

    Add tailwind

---
+++ src/lib/components/PlayerBar.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ProgressBar from './ProgressBar.svelte';
+</script>
+
+<div class="w-full bg-slate-700 h-20 border-t border-black flex justify-center items-center">
+	<div class="w-1/2">
+		<ProgressBar />
+	</div>
+</div>
--- src/routes/PlayAudio.svelte
+++ src/routes/PlayAudio.svelte
@@ -33,28 +33,3 @@
 {#if errorMessage != null}
 	<div style="color: red">{errorMessage}</div>
 {/if}
-
-<style>
-	.counter {
-		display: flex;
-		border-top: 1px solid rgba(0, 0, 0, 0.1);
-		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-		margin: 1rem 0;
-	}
-
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		touch-action: manipulation;
-		font-size: 2rem;
-	}
-
-	.counter button:hover {
-		background-color: var(--color-bg-1);
-	}
-</style>
